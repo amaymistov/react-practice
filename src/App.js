14b5fa6c4ee0224b04b134c6ea825ee48de92e2c
Robot <aomaymistov@gmail.com>
2023-01-22 22:03:33 +0300

    add cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
 function App() {
     return (<div className="wrapper clear">
+        <div className="overlay">
+            <div className="drawer d-flex flex-column">
+                <div className="d-flex align-center justify-between">
+                    <h2>Корзина</h2>
+                    <img className="removeButton" src="/img/removeBtn.svg" alt="Remove"/>
+                </div>
+                <div className="items d-flex flex-column">
+                    <div className="cartItem d-flex align-center">
+                        <img width={70} height={70} src="/img/sneakers/1.jpg" alt="1"/>
+                        <div>
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className="removeButton" src="/img/removeBtn.svg" alt="Remove"/>
+                    </div>
+                    <div className="cartItem d-flex align-center">
+                        <img width={70} height={70} src="/img/sneakers/2.jpg" alt="2"/>
+                        <div>
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className="removeButton" src="/img/removeBtn.svg" alt="Remove"/>
+                    </div>
+                </div>
+                <div className="cartTotalBlock">
+                    <ul className="d-flex flex-column">
+                        <li>
+                            <span>Итого: </span>
+                            <div></div>
+                            <b>21 498 руб. </b>
+                        </li>
+                        <li>
+                            <span>Налог 5%: </span>
+                            <div></div>
+                            <b>1074 руб. </b>
+                        </li>
+                    </ul>
+                    <button className="greenButton">Оформить заказ <img src="/img/arrow.svg" alt="Оформить заказ"/></button>
+                </div>
+            </div>
+        </div>
         <header className="d-flex justify-between align-center p-40">
             <div className="d-flex align-center">
                 <img width={40} height={40} src="/img/logo.png" alt="Sneakers shop"/>
